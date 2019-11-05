e2fe3d413230615944ee357849ed7ece9f55cb5e
ivyorca <55474425+ivyorca@users.noreply.github.com>
2019-11-05 13:59:12 +0800

    Update try_3.js

--- samples/weighted/try_3.js
+++ samples/weighted/try_3.js
@@ -1,4 +1,8 @@
 //document.getElementById("c_id").addEventListener('change', onSearchInput);
+var div = d3.select("body").append("div")
+.attr("class", "tooltip")
+.style("opacity", 1e-6);
+
 var c_id;
 var searchField;
 var root2;
@@ -10,12 +14,11 @@ function clearRoot2() {
 function filter() {
   c_id = document.getElementById("c_id").value;
   if (c_id) {
-    updateChart();
-
-    console.log(c_id);
-    searchField = "d.id";
     console.log(root1);
-    searchTree(root1);
+    searchField = "d.id";
+    var rootval = root1;
+    console.log(rootval);
+    searchTree(rootval);
   }
 }
 
@@ -216,7 +219,7 @@ function updateChart() {
 function draw(treeData, width, height) {
   var svg = d3.select("svg");
   // declares a tree layout and assigns the size
-  console.log(treeData);
+  // console.log(treeData);
   var treemap = d3.tree().size([width - 2, height - 2]);
   //  assigns the data to a hierarchy using parent-child relationships
   //console.log(treeData);
@@ -266,79 +269,89 @@ function draw(treeData, width, height) {
   //       d.parent.y
   //     );
   //   })
-    // .attr("stroke-width", function(d) {
-    //   var strokevar;
-    //   if ((d.data.data.value / 100) * 20 < 1) {
-    //     strokevar = 1;
-    //   } else {
-    //     strokevar = (d.data.data.value / 100) * 20;
-    //   }
-    //   return strokevar;
-    // });
+  // .attr("stroke-width", function(d) {
+  //   var strokevar;
+  //   if ((d.data.data.value / 100) * 20 < 1) {
+  //     strokevar = 1;
+  //   } else {
+  //     strokevar = (d.data.data.value / 100) * 20;
+  //   }
+  //   return strokevar;
+  // });
   //
-    var link = svg.select("g").selectAll(".link")
-        .data(nodes.descendants().slice(1))
-        .enter()
-        .append("g")
-        .attr("class", "link");
 
-    link.append("path")
-        .attr("fill", "none")
-        .attr("opacity", "0.7")
-        .attr("stroke-width", function(d) {
-          if(d.data.data.value){
-          var strokevar;
-          if ((d.data.data.value / 100) * 20 < 1) {
-            strokevar = 2.5;
-          } else {
-            strokevar = (d.data.data.value / 100) * 20;
-          }
-          return strokevar;}
-          else{
-            return 5;
-          }
-        })
-        .attr("d", function(d) {
-            return (
-              "M" +
-              d.x +
-              "," +
-              d.y +
-              "C" +
-              d.x +
-              "," +
-              (d.y + d.parent.y) / 2 +
-              " " +
-              d.parent.x +
-              "," +
-              (d.y + d.parent.y) / 2 +
-              " " +
-              d.parent.x +
-              "," +
-              d.parent.y
-            );
-          });
+  var div = d3
+    .select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0.8);
 
-    link.append("text")
-        .attr("font-family", "Arial, Helvetica, sans-serif")
-        .attr("fill", "Black")
-        .attr("stroke", "Black")
-        .style("font", "normal 12px Arial")
-        .attr("transform", function(d) {
-            return "translate(" +
-                ((d.x)) + "," +
-                ((d.y)) + ")";
-        })
-        .attr("dy", ".35em")
-        .attr("y", function(d) {
-          return d.children ? -40 : 35;
-        })
-        .attr("text-anchor", "middle")
-        .text(function(d) {
-          if(d.data.data.value){
-            console.log(d.data.data.value);
-             return (d.data.data.value + "%");}
-        });
+  var link = svg
+    .select("g")
+    .selectAll(".link")
+    .data(nodes.descendants().slice(1))
+    .enter()
+    .append("g")
+    .attr("class", "link");
+
+  link
+    .append("path")
+    .attr("fill", "none")
+    .attr("opacity", "0.7")
+    .attr("stroke-width", function(d) {
+      if (d.data.data.value) {
+        var strokevar;
+        if ((d.data.data.value / 100) * 30 < 1) {
+          strokevar = 2.5;
+        } else {
+          strokevar = (d.data.data.value / 100) * 30;
+        }
+        return strokevar;
+      } else {
+        return 30;
+      }
+    })
+    .attr("d", function(d) {
+      return (
+        "M" +
+        d.x +
+        "," +
+        d.y +
+        "C" +
+        d.x +
+        "," +
+        (d.y + d.parent.y) / 2 +
+        " " +
+        d.parent.x +
+        "," +
+        (d.y + d.parent.y) / 2 +
+        " " +
+        d.parent.x +
+        "," +
+        d.parent.y
+      );
+    });
+
+  link
+    .append("text")
+    .attr("font-family", "Arial, Helvetica, sans-serif")
+    .attr("fill", "Black")
+    .attr("stroke", "Black")
+    .style("font", "normal 12px Arial")
+    .attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")";
+    })
+    .attr("dy", ".35em")
+    .attr("y", function(d) {
+      return d.children ? -40 : 35;
+    })
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+      if (d.data.data.value) {
+        console.log(d.data.data.value);
+        return d.data.data.value + "%";
+      }
+    });
   // adds each node as a group
   var node = svg
     .select("g")
@@ -358,7 +371,11 @@ function draw(treeData, width, height) {
     });
 
   // adds the circle to the node
-  node.append("circle").attr("r", 10);
+  node.append("circle")
+  .on("mouseover",mouseover)
+  .on("mousemove",function(d){mousemove(d);})
+  .on("mouseout",mouseout)
+  .attr("r", 13);
 
   // adds the text to the node
   node
@@ -426,6 +443,27 @@ construct_generations = function(d) {
   console.log(generations);
   //  return generations;
 };
+
+//======================MOUSE EVENTS==================//
+function mouseover() {
+                div.transition()
+                .duration(300)
+                .style("opacity", 1);
+            }
+
+            function mousemove(d) {
+                div
+                .text("Company Name :" + d.data.data.id)
+                .style("left", (d3.event.pageX ) + "px")
+                .style("top", (d3.event.pageY) + "px");
+            }
+
+            function mouseout() {
+                div.transition()
+                .duration(300)
+                .style("opacity", 1e-6);
+            }
+
 
 va.messagingUtil.setOnDataReceivedCallback(onDataReceived);
 initChart();
